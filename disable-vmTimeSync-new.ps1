0cba57c89182125b9ebb7ae6dbd7f4b5738e9da8
chrisfederico1 <34996684+chrisfederico1@users.noreply.github.com>
2019-12-03 08:26:48 -0600

    Added more to new script

--- disable-vmTimeSync-new.ps1
+++ disable-vmTimeSync-new.ps1
@@ -1,12 +1,33 @@
-function cluster () {
+# The following script accepts input of SingleVM or Cluster Name.  
+# These settings disable the time sync to the VMhost . https://kb.vmware.com/s/article/1189
+
+
+function ClusterVMs ($Cluster,$ExtraValues) {
 
 
 
 }
 
-function SingleVM () {
+function SingleVM ($SingleVM,$ExtraValues) {
 
 
 
+}
 
+
+################ Script starts here ###############
+
+# Clear Screen
+Clear-Host
+
+# Promt User to select Single VM or Cluster Name
+write-host "1   Cluster"
+Write-host "2   Single VM"
+
+# Capture response from user
+$response = read-host "Enter 1 or 2"
+
+switch ($response)
+{
+    "1" {}
 }

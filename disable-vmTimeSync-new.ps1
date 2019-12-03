8be6126e4ee5f6c816fea27fc026fc9c81d934e9
chrisfederico1 <34996684+chrisfederico1@users.noreply.github.com>
2019-12-03 13:44:48 -0600

    Cluster function almost complete.

--- disable-vmTimeSync-new.ps1
+++ disable-vmTimeSync-new.ps1
@@ -2,15 +2,56 @@
 # These settings disable the time sync to the VMhost . https://kb.vmware.com/s/article/1189
 
 
-function ClusterVMs ($Cluster,$ExtraValues) {
+function ClusterVMs ($ExtraValues) {
+
+	# Get Cluster Name
+	$Cluster = read-host 'Input Cluster Name'
+	Write-host "Warning : You are about to add advanced Time Sync Settings to all VMs in the following Cluster: $Cluster" -BackgroundColor Red
+
+	$reply = read-host "Continue?[y/n]"
+	if ($reply -match "[nN]")
+	{
+	exit 
+	}
+
+	# Get vms in Cluster
+try { 
+	$vmlist = get-cluster -Name $Cluster | get-vm
+	}
+	catch {
+		Write-host "An Error Occured:"
+		write-host $_.ScriptStackTrace
+	}
+
+	foreach ($vm in $vmlist)
 
 
+	{
+		$vmview = get-view -ViewType VirtualMachine -Filter @{"Name" = $vm.Name} #| where-object {-not $_.config.template}
+		
+		$vmConfigSpec = new-object VMware.Vim.VirtualMachineConfigSpec
+		
+		# Gathering settings and prepare for adding it to virtual machine settings
+		
+		$ExtraValues | ForEach-Object GetEnumerator | ForEach-Object {
+			$extra = New-object VMware.Vim.OptionValue
+			$extra.key = $_.key
+			$extra.value=$_.value
+			$vmConfigSpec.ExtraConfig += $extra
+		}
+			# Performing commit to Virtual Machine.
+			write-host "Reconfiguring " $vm.Name
+			$vmview.ReconfigVM($vmConfigSpec)
+		
+	}
 
 }
 
-function SingleVM ($SingleVM,$ExtraValues) {
+function SingleVM ($ExtraValues) {
 
+	
 
+$vmview = get-view -ViewType VirtualMachine -Filter @{"Name" = "D1WFSERVMGT01"} #| where-object {-not $_.config.template}
 
 }
 
@@ -20,14 +61,14 @@ function SingleVM ($SingleVM,$ExtraValues) {
 # Clear Screen
 Clear-Host
 
-# Promt User to select Single VM or Cluster Name
+# Prompt User to select Single VM or Cluster Name
 write-host "1   Cluster"
 Write-host "2   Single VM"
 
 # 1 Blank line
 ""
 
-# Creating a hash table with key-values for the timesync advanced settings
+# Creating a hash table with key-values for disable time sync advanced settings
 $ExtraValues = @{
 	"tools.syncTime"="0";
 	"time.synchronize.continue"= "0";
